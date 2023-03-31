14f7e2b069e63d4b62c81f7517fe9813d3a0a882
Erin M. Richards <80139666+erin-m-richards@users.noreply.github.com>
2023-03-31 15:33:29 -0600

    Fix output to be (x,y) with each row as a position.

--- txt2positions.m
+++ txt2positions.m
@@ -7,19 +7,19 @@ function nucleusPos = txt2positions(txtFile)
 % txtFile: .txt file that you want to read in
 %   string
 
-outlineFileName = fopen(txtFile);
-line = fgetl(outlineFileName);
+filename = fopen(txtFile);
+line = fgetl(filename);
 numNuclei = 1;
 
 while ischar(line) % As long as the file gives characters...
     %Find rows with the nuclear position arrays.
     nucleusTitleRow = strfind(line, 'Nucleus_START');
     if length(nucleusTitleRow) == 1
-        line = fgetl(outlineFileName); % Move down to X pos
+        line = fgetl(filename); % Move down to X pos
         Xchar = line; % Get line
         nucleusX = strsplit(Xchar); % Split at spaces
         nucleusX = nucleusX(2:end-1); % Remove header
-        line = fgetl(outlineFileName); % Move down to y pos
+        line = fgetl(filename); % Move down to y pos
         Ychar = line;
         nucleusY = strsplit(Ychar);
         nucleusY = nucleusY(2:end-1);
@@ -31,6 +31,6 @@ while ischar(line) % As long as the file gives characters...
         nucleusPos{numNuclei, 1} = nucleusList;
         numNuclei = numNuclei + 1;
     end
-    line = fgetl(outlineFileName); % Move down a line
+    line = fgetl(filename); % Move down a line
 end
-fclose(outlineFileName);
+fclose(filename);
